f1e7ebb4f9c275012ca84e8c83f1d91dcb18e0ba
Shreedeep <neoegoist011@gmail.com>
2026-08-09 11:43:45 +0530

    no changes

--- test_libs.py
+++ test_libs.py
@@ -3,6 +3,6 @@ import numpy as np
 import scipy
 import cv2
 
-print(f"✅ NumPy  : {np.__version__}")
-print(f"✅ SciPy  : {scipy.__version__}")
-print(f"✅ OpenCV : {cv2.__version__}")
+print(f" NumPy  : {np.__version__}")
+print(f" SciPy  : {scipy.__version__}")
+print(f" OpenCV : {cv2.__version__}")
