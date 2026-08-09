7967314e51d1f31c40430783a6c12a2ea52a6ba9
Shreedeep <neoegoist011@gmail.com>
2026-08-09 11:41:15 +0530

    test camera error fixed

--- test_camera.py
+++ test_camera.py
@@ -5,16 +5,16 @@ from config import PHONE_STREAM_URL
 cap = cv2.VideoCapture(PHONE_STREAM_URL)
 
 if not cap.isOpened():
-    print("❌ Cannot connect to phone camera.")
+    print(" Cannot connect to phone camera.")
     print("   → Check if phone and laptop are on same Wi-Fi")
     print("   → Check if IP Webcam server is running on phone")
 else:
-    print("✅ Connected to phone camera!")
+    print(" Connected to phone camera!")
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ Failed to grab frame.")
+        print(" Failed to grab frame.")
         break
 
     cv2.imshow("Phone Camera Feed - Press Q to quit", frame)
